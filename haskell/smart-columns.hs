7df637082816f3f0407b853d15e5044438f4f1c3
Marc Chantreux <khatar@phear.org>
2016-01-22 23:39:37 +0100

    just a bit shorter
    also added data file to test

--- haskell/smart-columns.hs
+++ haskell/smart-columns.hs
@@ -5,9 +5,8 @@ pad n s | r > 0     = s ++ replicate r ' '
         | otherwise = s
         where r = n - length s
 
-pad_cells sheet =
-    let ns= map (maximum . map length) $ transpose sheet
-    in map (zipWith pad ns) sheet
+reformat sheet =
+    let ns= map (maximum . map length)  $ transpose sheet
+    in map (concat . intersperse " # ") $ map (zipWith pad ns) sheet
 
-reformat = map (concat . intersperse " # ") . pad_cells
 main = interact $ unlines . reformat . (map (splitOn "#") ) . lines
