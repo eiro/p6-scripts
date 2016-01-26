bdbef5fed0df8d6f7db4070a6fd10f013562b817
Marc Chantreux <khatar@phear.org>
2016-01-26 12:18:48 +0100

    smart-columns first parameter to set the separator

--- haskell/smart-columns.hs
+++ haskell/smart-columns.hs
@@ -1,12 +1,17 @@
 import Data.List.Split (splitOn)
 import Data.List
+import System.Environment
 
 pad n s | r > 0     = s ++ replicate r ' '
         | otherwise = s
         where r = n - length s
 
-reformat sheet =
-    let ns= map (maximum . map length)  $ transpose sheet
-    in map (concat . intersperse " # ") $ map (zipWith pad ns) sheet
-
-main = interact $ unlines . reformat . (map $ splitOn "#") . lines
+main = do
+    args <- getArgs
+    let sep     = (args ++ ["#"]) !! 0
+        between = " " ++ sep ++ " "
+        reformat sheet =
+            let ns= map (maximum . map length)    $ transpose sheet
+            in map (concat . intersperse between) $ map (zipWith pad ns) sheet
+    print sep
+    interact $ unlines . reformat . (map $ splitOn sep) . lines
