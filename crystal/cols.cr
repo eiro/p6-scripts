5552f84870c29d5776943bddec5428bc9aa8a15d
Marc Chantreux <khatar@phear.org>
2018-07-19 18:55:20 +0200

    still less chars :)

--- crystal/cols.cr
+++ crystal/cols.cr
@@ -1,11 +1,11 @@
-ss = STDIN
-    .gets_to_end
-    .split("\n")
-    .map( &.split(':') )
-    .tap( &.pop )
-    .transpose
-    .map { |col|
-        fmt = "%-#{ col.map(&.size).max }s"
-        col.map! { |cell| fmt % cell } }
-    .transpose
-    .each { |line| STDOUT << line.join(';') << '\n' }
+STDIN
+.gets_to_end
+.split("\n")
+.map( &.split(ARGV[0]) )
+.tap( &.pop )
+.transpose
+.map { |col|
+    fmt = "%-#{ col.map(&.size).max }s"
+    col.map! { |cell| fmt % cell } }
+.transpose
+.each { |line| STDOUT << line.join(';') << '\n' }
