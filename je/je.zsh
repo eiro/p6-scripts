32f26274af79142fe6e2b1b21ea3a097162b84d5
Marc Chantreux <khatar@phear.org>
2018-12-21 11:29:23 +0100

    add vipe as editor

--- je/je.zsh
+++ je/je.zsh
@@ -1,14 +1,6 @@
-je/travaille () {awk -F"\t" -vdesc=2 '{print $desc}' "$@"|sort -u}
-je/continue  () { je/travaille ~/.je | fzf | read it }
-je/commence () {
-	# well ... pretty sure i can uze a zsh widget there
-	local tmp=
-	mktemp | read tmp
-	echo "$*" > $tmp
-	${EDITOR:-vim} $tmp
-	read it < $tmp
-	rm -rf $tmp
-}
+je/travaille () { awk -F"\t" -vdesc=2 '{print $desc}' "$@"|sort -u }
+je/continue  () { je/travaille ~/.je | fzf  | read it }
+je/commence  () { echo "$*"          | vipe | read it }
 je () {
 	local it
 	if (( $+1 )) { je/commence "$*" } else { je/continue }
