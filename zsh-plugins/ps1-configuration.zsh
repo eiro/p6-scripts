7a623b455267eb6727f217fdc2e1ffbf0d7488b5
Marc Chantreux <khatar@phear.org>
2019-04-01 10:19:48 +0200

    starting a progress bar for je

--- zsh-plugins/ps1-configuration.zsh
+++ zsh-plugins/ps1-configuration.zsh
@@ -18,6 +18,18 @@ ps1-update () {
 		set -- ~/local/mail/^Trash/new/*(N:h:h:t)
 		test -n "$*" && echo $( l $@ | uniq -c ))"
 	local it= branch=
+	local doing_this="$( tac ~/.je | sed  's/.\{17\}//;q' )"
+	if [[ stop = $doing_this ]] {
+		doing_this="%B%K{red}%F{white}▒ NO ACTIVE TASK ▒%f%k%b"
+	} else {
+		# TODO: make it a library
+		local graph_range=.▁▂▃▄▅▆▇█
+		# digraphs lb et RB
+		# this is a fake remaining bar just to let you remember
+		# how awesome it could be to have one!
+		doing_this="▐%F{green}▒▒▒▒%F{gray}▒▒▌%f $doing_this"
+	}
+
 	PS2="▒░ ▷ "
 	it+="▒░ %(?.%F{green}$icons[OK_ICON].%F{red}$icons[FAIL_ICON])%f"
 	it+="${TMUX_PANE:+ 📺${TMUX_PANE#%} }${GOTMAIL:+ 📨 }"
