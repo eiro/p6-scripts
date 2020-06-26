452d2fc9c7f8e4ff82102d019d386613abe69ed6
Marc Chantreux <eiro@phear.org>
2020-06-26 12:15:26 +0200

    fix fstab usage

--- zsh-plugins/netdrive.zsh
+++ zsh-plugins/netdrive.zsh
@@ -5,21 +5,15 @@ netdrive/ssh () sshfs $1:t:r\: "$@"
 
 netdrive/umount () {
 	[[ $1 = - ]] && 1=${OLDPWD?no previous path}
-    # lazy unmount
 	fusermount -uz ${1}(:A)
 }
 
 netdrive/status () {
 	[[ $1 = - ]] && 1=${OLDPWD?no previous path}
-    lsof +f -- "$@"
+	lsof +f -- "$@"
 }
 
-netdrive/fstab () {
-	local fstab=$1 name=$2
-	local dir=$fstab:h/$name
-	local cmd="$( sed -n s/^$name //p )"
-	test -n "$cmd" && eval $cmd
-}
+netdrive/fstab () eval "$( sed -n "s;^$1:t ;;p" $fstab )"
 
 netdrive/mount () {
 	1=($1(:A))
@@ -27,8 +21,9 @@ netdrive/mount () {
 		netdrive/$1:e "$@"
 		return
 	}
-	@ ( $1:h/fstab(N) ) {
-		netdrive/fstab $it $1 ||
+	local fstab
+	for fstab ( $1:h/fstab(N) ) {
+		netdrive/fstab $1 ||
 			warn "can't mount $1"
 	}
 }
