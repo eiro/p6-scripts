0e7d3cb66493cf92fb8433ab4ea3c2ef2787f4c1
Marc Chantreux <eiro@phear.org>
2020-06-26 12:16:46 +0200

    remove trailing spaces

--- zsh-plugins/breadcrumb.zsh
+++ zsh-plugins/breadcrumb.zsh
@@ -1,6 +1,6 @@
 breadcrumb/colored () {
 	while (( $# )) {
-		it+="%F{$1:h}%K{$1:t}$2%F{$1:t}" 
+		it+="%F{$1:h}%K{$1:t}$2%F{$1:t}"
 		if (( $# > 3 )) {
 			it+="%K{$3:t}$icons[LEFT_SEGMENT_SEPARATOR]"
 		} else {
