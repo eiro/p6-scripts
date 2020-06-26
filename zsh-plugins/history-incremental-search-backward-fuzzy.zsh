becb00ac9ec1318d223e7017ac0af58244ace3f1
Marc Chantreux <eiro@phear.org>
2020-06-26 12:19:26 +0200

    sk to fzy

--- zsh-plugins/history-incremental-search-backward-fuzzy.zsh
+++ zsh-plugins/history-incremental-search-backward-fuzzy.zsh
@@ -1,9 +1,8 @@
 # when history-incremental-search-backward is not enough
 
+# fzy replaces sk --tac --tiebreak=index
 history-incremental-search-backward-fuzzy () BUFFER=$(
-    fc -ln 0 |
-        awk '!f[$0]++' |
-        sk --tac --tiebreak=index
+    fc -ln 0 | awk '!f[$0]++' |tac|fzy
 )
 
 zle -N history-incremental-search-backward-fuzzy
