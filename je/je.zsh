25deabfae9a7cd6f0a0a26c880f118ebfb65fa64
Marc Chantreux <eiro@phear.org>
2020-06-26 12:23:28 +0200

    improve user experience

--- je/je.zsh
+++ je/je.zsh
@@ -1,10 +1,27 @@
 # vim: noet ts=4
 
-je/travaille () { awk -F"\t" -vdesc=2 '{print $desc}' "$@"|sort -u }
-je/continue  () { je/travaille ~/.je | fzf  | read it }
-je/commence  () { echo "$*"          | vipe | read it }
+je/liste () {
+	awk -F"\t" -vdesc=2 '{print $desc}' "$@"|sort -u
+}
+
+je/continue  () {
+	je/liste ~/.je     |
+	fzf                |
+	read it
+}
+
+je/commence  () {
+	echo "$*" |
+	vipe      |
+	read it
+}
+
 je () {
 	local it
-	if (( $+1 )) { je/commence "$*" } else { je/continue }
+	if (( $+1 )) {
+		je/commence "$*"
+	} else {
+		je/continue
+	}
 	test -n "$it" && date +"%F %H:%M	$it" >> ~/.je
 }
