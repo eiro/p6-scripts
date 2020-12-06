d7b50299de0ed266e1bbc6a705215f2888ad56f3
Marc Chantreux <eiro@phear.org>
2020-12-06 20:44:21 +0100

    add je_sum() to create a report

--- je/je.zsh
+++ je/je.zsh
@@ -13,18 +13,25 @@ je () {
 		date +"%F %H:%M	$it" >> ~/.je
 }
 
-je/commence  () {
-	echo "$*" |
-	vipe      |
-	read it
-}
-
-je () {
-	local it
-	if (( $+1 )) {
-		je/commence "$*"
-	} else {
-		je/continue
-	}
-	test -n "$it" && date +"%F %H:%M	$it" >> ~/.je
+je_sum () {
+    the=$(mktemp -d)
+    mkfifo $the/report $the/tasks
+    < $the/report cut -f2 > $the/tasks&
+    cat "$@"                |
+        tee $the/report     |
+        cut -f1             |
+        date +%s -f-        |
+        paste - $the/tasks  |
+        awk -F'\t' -vOFS='\t' -vstop=stop -vOFMT='%.3g' -vts=1 -vtask=2 '
+            start {
+                did[cur] += $ts - start
+                if ( $task ~ stop ) {
+                    start=cur="" ;
+                    next
+                }
+            }
+            { start=$ts ; cur=$task }
+            END { for (d in did) print d,did[d]/3600 }
+        '
+    rm -rf $the
 }
