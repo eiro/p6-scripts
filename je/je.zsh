e202db8eb42edb7229f381dd50052478afe6fd5a
Marc Chantreux <eiro@phear.org>
2020-12-08 10:55:42 +0100

    coding style only
    * indent with tabs instead of spaces
    * new lines after pipes

--- je/je.zsh
+++ je/je.zsh
@@ -1,6 +1,9 @@
 # vim: noet ts=4
 
-je_liste () { cut -f2 -d"	" "$@" | sort -u ; }
+je_liste () {
+	cut -f2 -d"	" "$@" |
+		sort -u
+}
 
 je () {
 	local it=$(
@@ -15,23 +18,23 @@ je () {
 
 je_sum () {
 	local the=$(mktemp -d)
-    mkfifo $the/report $the/tasks
-    < $the/report cut -f2 > $the/tasks&
+	mkfifo $the/report $the/tasks
+	< $the/report cut -f2 > $the/tasks&
 	sed '/^ *[^#]/!d' "$@"  |
-        tee $the/report     |
-        cut -f1             |
-        date +%s -f-        |
-        paste - $the/tasks  |
-        awk -F'\t' -vOFS='\t' -vstop=stop -vOFMT='%.3g' -vts=1 -vtask=2 '
-            start {
-                did[cur] += $ts - start
-                if ( $task ~ stop ) {
-                    start=cur="" ;
-                    next
-                }
-            }
-            { start=$ts ; cur=$task }
-            END { for (d in did) print d,did[d]/3600 }
-        '
-    rm -rf $the
+		tee $the/report     |
+		cut -f1             |
+		date +%s -f-        |
+		paste - $the/tasks  |
+		awk -F'\t' -vOFS='\t' -vstop=stop -vOFMT='%.3g' -vts=1 -vtask=2 '
+			start {
+				did[cur] += $ts - start
+				if ( $task ~ stop ) {
+					start=cur="" ;
+					next
+				}
+			}
+			{ start=$ts ; cur=$task }
+			END { for (d in did) print d,did[d]/3600 }
+		'
+	rm -rf $the
 }
